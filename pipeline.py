3a9ba13af8b6c690c2b468e9cffb9d71e4b94a3d
Zayd Patel <zayd62@gmail.com>
2020-02-11 14:44:23 +0000

    completed crawl category stage of the pipeline

--- pipeline.py
+++ pipeline.py
@@ -13,42 +13,53 @@ PRODUCT_SAVE = "items.csv"
 # 2: category name
 
 # crawl the category
-category_url = None
-
-# create crawler process for category crawling 
-process = CrawlerProcess(
-    settings={
-        "FEED_FORMAT": "json",
-        "FEED_URI": "items.json",
-        "USER_AGENT": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36",
-        "DOWNLOAD_DELAY": "1",
-        "AUTOTHROTTLE_ENABLED": "True",
-        "HTTPCACHE_ENABLED": "False",
-    }
-)
+category_url = sys.argv[1]
+category_name = sys.argv[2]
 
 # open database session and make it available to the crawler
 session = Session()
-# CrawlCategory.dbSession = session
 
-# # url to scrape
-# url = sys.argv[1]
-# CrawlCategory.start_urls = [url]
 
-# # create category object and add to session
-# catName = sys.argv[2]
-# cat = Category(catName)
-# session.add(cat)
+def crawl_category(session, url, catName):
+    """
+    Given a url, the function will crawl the category and insert the crawled pages into the database. 
+    it will also return the generate category object (using catName to name the category) from which you are able to determine
+    the page entries created in the database due to the one-to-many relationship between the category and the page
+    """
+    # create crawler process for category crawling
+    process = CrawlerProcess(
+        settings={
+            "FEED_FORMAT": "json",
+            "FEED_URI": "items.json",
+            "USER_AGENT": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36",
+            "DOWNLOAD_DELAY": "1",
+            "AUTOTHROTTLE_ENABLED": "True",
+            "HTTPCACHE_ENABLED": "False",
+        }
+    )
 
-# # make category object available to the crawler
-# CrawlCategory.catObject = cat
+    # Give the crawler access to the database session
+    CrawlCategory.dbSession = session
 
-# # run the crawler
-# print("starting crawl for category")
-# process.crawl(CrawlCategory)
-# process.start()  # the script will wait here until the crawling is complete
-# print("finished crawl for category. Committing the change to the database")
-# session.commit()
+    # Give the crawler the url to scrape
+    CrawlCategory.start_urls = [url]
+
+    # create category object and add to session
+    cat = Category(catName)
+    session.add(cat)
+
+    # Give the crawler the category object
+    CrawlCategory.catObject = cat
+
+    # run the crawler
+    print("starting crawl for category")
+    process.crawl(CrawlCategory)
+    process.start()  # the script will wait here until the crawling is complete
+    print("finished crawl for category. Committing the change to the database")
+        
+    # save the results of the crawling to the database
+    session.commit()
+    return cat
 
 print("Starting page --> product HTML parse")
 parse(session)
@@ -65,3 +76,6 @@ process = CrawlerProcess(
         "HTTPCACHE_ENABLED": "False",
     }
 )
+
+
+create_category_object = crawl_category()

4343e000f95775d0246d65f01bc965ccb70b162d
Zayd <zayd62@gmail.com>
2020-05-12 18:42:45 +0100

    fully recovered

--- Rmarkdown Report/_build.sh
+++ Rmarkdown Report/_build.sh
@@ -2,5 +2,5 @@
 
 set -ev
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
-
+texcount "/Common/University/Year 3/Final Year Project/git-repository/Rmarkdown Report/_book/Translating-Natural-Language-Queries-into-SQL.tex"
 
